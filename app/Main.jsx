7c72ad3ac11e1f0eb0087ccdf01135820c963d94
Wes Marc <weslei047@gmail.com>
2025-06-19 03:08:22 -0300

    feat: Add create post logic and route

--- app/Main.jsx
+++ app/Main.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Axios from "axios";
+Axios.defaults.baseURL = "http://localhost:8080";
 
 // Components
 import ReactDOM from "react-dom/client";
