c68f1c34887b26a0b43a394c96750f6082dc23d0
Wes Marc <weslei047@gmail.com>
2025-07-09 00:37:02 -0300

    fix: Remove duplicated search-overlay div

--- app/components/Search.jsx
+++ app/components/Search.jsx
@@ -16,7 +16,7 @@ function Search() {
     }
 
     return (
-        <div className="search-overlay">
+        <>
             <div className="search-overlay-top shadow-sm">
                 <div className="container container--narrow">
                     <label htmlFor="live-search-field" className="search-overlay-icon">
@@ -52,7 +52,7 @@ function Search() {
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
