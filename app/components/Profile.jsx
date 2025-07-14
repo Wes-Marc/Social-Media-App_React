f240381d007aa5ad49a4a8a128f86b862ca61f20
Wes Marc <weslei047@gmail.com>
2025-07-13 21:56:55 -0300

    feat: Add follow/unfollow button logic

--- app/components/Profile.jsx
+++ app/components/Profile.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useContext, useState } from "react";
+import React, { useEffect, useContext } from "react";
+import { useImmer } from "use-immer";
 import Axios from "axios";
 import { useParams } from "react-router-dom";
 import StateContext from "../StateContext";
@@ -8,47 +9,138 @@ import ProfilePosts from "./ProfilePosts";
 function Profile() {
     const appState = useContext(StateContext);
     const { username } = useParams();
-    const [profileData, setProfileData] = useState({
-        profileUsername: "...",
-        profileAvatar: "https://gravatar.com/avatar/placeholder?s=128",
-        isFollowing: false,
-        counts: {
-            postCount: "",
-            followerCount: "",
-            followingCount: ""
+    const [state, setState] = useImmer({
+        followActionLoading: false,
+        startFollowingRequestCount: 0,
+        stopFollowingRequestCount: 0,
+        profileData: {
+            profileUsername: "...",
+            profileAvatar: "https://gravatar.com/avatar/placeholder?s=128",
+            isFollowing: false,
+            counts: {
+                postCount: "",
+                followerCount: "",
+                followingCount: ""
+            }
         }
     });
 
+    // Loads Profile Data
     useEffect(() => {
+        const requestController = new AbortController();
+
         async function fetchData() {
             try {
-                const response = await Axios.post(`/profile/${username}`, { token: appState.user.token });
-                setProfileData(response.data);
+                const response = await Axios.post(`/profile/${username}`, { token: appState.user.token }, { signal: requestController.signal });
+                setState(draft => {
+                    draft.profileData = response.data;
+                });
             } catch (error) {
                 console.log(error.response.data);
             }
         }
         fetchData();
-    }, []);
+
+        return () => {
+            requestController.abort();
+        };
+    }, [username]);
+
+    // Logic for Start Follow Feature
+    useEffect(() => {
+        if (state.startFollowingRequestCount) {
+            setState(draft => {
+                draft.followActionLoading = true;
+            });
+
+            const requestController = new AbortController();
+
+            async function fetchData() {
+                try {
+                    const response = await Axios.post(`/addFollow/${state.profileData.profileUsername}`, { token: appState.user.token }, { signal: requestController.signal });
+                    setState(draft => {
+                        draft.profileData.isFollowing = true;
+                        draft.profileData.counts.followerCount++;
+                        draft.followActionLoading = false;
+                    });
+                } catch (error) {
+                    console.log(error.response.data);
+                }
+            }
+            fetchData();
+
+            return () => {
+                requestController.abort();
+            };
+        }
+    }, [state.startFollowingRequestCount]);
+
+    // Logic for Stop Follow Feature
+    useEffect(() => {
+        if (state.stopFollowingRequestCount) {
+            setState(draft => {
+                draft.followActionLoading = true;
+            });
+
+            const requestController = new AbortController();
+
+            async function fetchData() {
+                try {
+                    const response = await Axios.post(`/removeFollow/${state.profileData.profileUsername}`, { token: appState.user.token }, { signal: requestController.signal });
+                    setState(draft => {
+                        draft.profileData.isFollowing = false;
+                        draft.profileData.counts.followerCount--;
+                        draft.followActionLoading = false;
+                    });
+                } catch (error) {
+                    console.log(error.response.data);
+                }
+            }
+            fetchData();
+
+            return () => {
+                requestController.abort();
+            };
+        }
+    }, [state.stopFollowingRequestCount]);
+
+    function startFollowing() {
+        setState(draft => {
+            draft.startFollowingRequestCount++;
+        });
+    }
+
+    function stopFollowing() {
+        setState(draft => {
+            draft.stopFollowingRequestCount++;
+        });
+    }
 
     return (
         <Page title="Profile Screen">
             <h2>
-                <img className="avatar-small" src={profileData.profileAvatar} /> {profileData.profileUsername}
-                <button className="btn btn-primary btn-sm ml-2">
-                    Follow <i className="fas fa-user-plus"></i>
-                </button>
+                <img className="avatar-small" src={state.profileData.profileAvatar} /> {state.profileData.profileUsername}
+                {appState.loggedIn && !state.profileData.isFollowing && appState.user.username != state.profileData.profileUsername && state.profileData.profileUsername != "..." && (
+                    <button onClick={startFollowing} disabled={state.followActionLoading} className="btn btn-primary btn-sm ml-2">
+                        Follow <i className="fas fa-user-plus"></i>
+                    </button>
+                )}
+                {appState.loggedIn && state.profileData.isFollowing && appState.user.username != state.profileData.profileUsername && state.profileData.profileUsername != "..." && (
+                    <button onClick={stopFollowing} disabled={state.followActionLoading} className="btn btn-danger btn-sm ml-2">
+                        Stop Following <i className="fas fa-user-times"></i>
+                    </button>
+                )}
             </h2>
 
             <div className="profile-nav nav nav-tabs pt-2 mb-4">
                 <a href="#" className="active nav-item nav-link">
-                    Posts: {profileData.counts.postCount}
+                    Posts: {state.profileData.counts.postCount}
                 </a>
                 <a href="#" className="nav-item nav-link">
-                    Followers: {profileData.counts.followerCount}
+                    Followers: {state.profileData.counts.followerCount}
                 </a>
                 <a href="#" className="nav-item nav-link">
-                    Following: {profileData.counts.followingCount}
+                    Following: {state.profileData.counts.followingCount}
                 </a>
             </div>
 
